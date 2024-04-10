2e6e9acf85bc5244177dbc3bc5f15a4479ec5132
Huda Mahmood <z5390834@ad.unsw.edu.au>
2024-04-10 23:41:18 +1000

    Created NavBar, added default assets

--- portfolio-react-app/src/App.js
+++ portfolio-react-app/src/App.js
@@ -1,7 +1,12 @@
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+
 function App() {
   return (
     <div>
-      
+      {/* if you take it out of this order then it all stuffs up */}
+      <NavBar />
+      <Home />
     </div>
   );
 }
