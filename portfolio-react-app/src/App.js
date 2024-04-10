819d524e319724bcebb9161969b6a4f1491d8955
Huda Mahmood <z5390834@ad.unsw.edu.au>
2024-04-11 01:35:32 +1000

    Created Projects, need to update links to demo and code

--- portfolio-react-app/src/App.js
+++ portfolio-react-app/src/App.js
@@ -1,6 +1,7 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Skills from "./components/Skills";
+import Projects from "./components/Projects";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <NavBar />
       <Home />
       <Skills />
+      <Projects />
     </div>
   );
 }
