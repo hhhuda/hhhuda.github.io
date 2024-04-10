2fd229eea76eff9457053f261bae12c0e627a945
Huda Mahmood <z5390834@ad.unsw.edu.au>
2024-04-11 00:28:51 +1000

    Created Home, decided to delete About section

--- portfolio-react-app/src/components/Home.jsx
+++ portfolio-react-app/src/components/Home.jsx
@@ -3,8 +3,22 @@ import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#031428]'>
-        
+    <div name='home' className='w-full h-screen bg-[#031428]'>
+        { /* container */ }
+        <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+            <p className='text-pink-600'>Hello, my name is</p>
+            <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Huda Mahmood</h1>
+            <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I'm a Full Stack Developer</h2>
+            <p className='text-[#889260] py-4 max-w-[700px]'>I'm a blah blah lalallalalalalallala</p>
+            <div>
+                <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>View Work
+                {/* adding group to both the button and arrow make it so the rotate occurs when you hover over the button, not just the arrow */}
+                <span className='group-hover:rotate-90 duration-300'>
+                    <HiArrowNarrowRight className='ml-3'/>
+                </span>
+                </button>
+            </div>
+        </div>
     </div>
   )
 }
---
+++ portfolio-react-app/src/components/Skills.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+import HTML from '../assets/html.png';
+import CSS from '../assets/css.png';
+import JavaScript from '../assets/javascript.png';
+import ReactImg from '../assets/react.png';
+import Node from '../assets/node.png';
+import FireBase from '../assets/firebase.png';
+import GitHub from '../assets/github.png';
+import Tailwind from '../assets/tailwind.png';
+import Mongo from '../assets/mongo.png';
+
+const Skills = () => {
+  return (
+    <div name='skills'>
+        {/* container */}
+        <div>
+            <div>
+                <p>Experience</p>
+                <p>These are the technologies that I have worked with:</p>
+            </div>
+
+            <div>
+                <div>
+                    <img
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Skills
