0a88cfe3e67d4bce889d4eec294e8e91c257f464
Huda Mahmood <z5390834@ad.unsw.edu.au>
2024-04-11 00:55:36 +1000

    Created Skills, need to update images to match

--- portfolio-react-app/src/App.js
+++ portfolio-react-app/src/App.js
@@ -1,5 +1,6 @@
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import Skills from "./components/Skills";
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       {/* if you take it out of this order then it all stuffs up */}
       <NavBar />
       <Home />
+      <Skills />
     </div>
   );
 }
--- portfolio-react-app/src/components/Skills.jsx
+++ portfolio-react-app/src/components/Skills.jsx
@@ -12,17 +12,47 @@ import Mongo from '../assets/mongo.png';
 
 const Skills = () => {
   return (
-    <div name='skills'>
+    <div name='skills' className='w-full h-screen bg-[#031428] text-gray-300'>
         {/* container */}
-        <div>
+        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
             <div>
-                <p>Experience</p>
-                <p>These are the technologies that I have worked with:</p>
+                <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Skills</p>
+                <p className='py-4'>These are the technologies that I have worked with:</p>
             </div>
 
-            <div>
-                <div>
-                    <img
+            {/* container for all our icons */}
+            <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt="Java icon" />
+                    <p className='my-4'>Java</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt="C icon" />
+                    <p className='my-4'>C</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt="Python icon" />
+                    <p className='my-4'>Python</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={JavaScript} alt="JavaScript icon" />
+                    <p className='my-4'>JavaScript</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={HTML} alt="HTML icon" />
+                    <p className='my-4'>HTML</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={CSS} alt="CSS icon" />
+                    <p className='my-4'>CSS</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={ReactImg} alt="React icon" />
+                    <p className='my-4'>React</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-20 mx-auto' src={Tailwind} alt="Tailwind CSS icon" />
+                    <p className='my-4'>Tailwind CSS</p>
                 </div>
             </div>
         </div>
